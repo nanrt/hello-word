6c81119a10c98657f7c685ac13aa066edcd59208
nanrt <1686886012@qq.com>
2019-04-08 15:08:39 +0800

    master提交

---
+++ src/main/java/com/channelsoft/po/Test.java
@@ -0,0 +1,33 @@
+package com.channelsoft.po;
+
+/**
+ * <dl>
+ * <dt>Test</dt>
+ * <dd>Description:</dd>
+ * <dd>Copyright: Copyright (C) 2006</dd>
+ * <dd>Company: 青牛（北京）技术有限公司</dd>
+ * <dd>CreateDate: 2019-04-08 15:07</dd>
+ * </dl>
+ *
+ * @author nanrt
+ */
+public class Test {
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
